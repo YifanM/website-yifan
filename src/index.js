3c8d59da8b4b2b3ef09c65a925687063e642231e
YifanM <yifan.ma@hotmail.com>
2017-04-25 03:55:42 -0400

    changed to angular2

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Page from './Page.js';
-import './index.css';
-
-ReactDOM.render(
-  <Page />,
-  document.getElementById('root')
-);
