19eb5af82da3034d8aa5f647f9dd3be46996459d
YifanM <yifan.ma@hotmail.com>
2017-10-09 16:50:13 -0400

    removed hash history
    added configuration to server to always redirect to index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -67,3 +67,15 @@ const appRoutes: Routes = [
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/* 
+This was added to .htaccess to configure the Apache server:
+
+# Don't rewrite files or directories
+RewriteCond %{REQUEST_FILENAME} -f [OR]
+RewriteCond %{REQUEST_FILENAME} -d
+RewriteRule ^ - [L]
+
+# Rewrite everything else to index.html to allow html5 state links
+RewriteRule ^ index.html [L]
+*/
