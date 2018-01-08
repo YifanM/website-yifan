8687d4222f0b3454ff44def6bcb598b974f0843a
YifanM <yifan.ma@hotmail.com>
2018-01-08 01:20:46 -0500

    updated content

--- src/app/otherwork/app.otherwork.ts
+++ src/app/otherwork/app.otherwork.ts
@@ -5,7 +5,8 @@ import { Component } from '@angular/core';
 declare interface IWorkItem {
 	id: string;
 	title: string;
-	timeline: string;
+	jobTitle?: string;
+	timeline?: string;
 	description: string;
 	technologies: string;
 	isOpen?: boolean;
@@ -25,45 +26,44 @@ export class OtherWorkComponent {
 		{
 			id: "workTS",
 			title: "TribalScale",
+			jobTitle: "Agile Software Engineer",
 			timeline: "September 2017 - December 2017 (2B)",
-			description: "",
-			technologies: "",
-			imageUrl: ""
+			description: `I was able to work on various projects such as NBA's Alexa skill, Carnival's Android app, and CIBC's website.
+			I was also able to work on TribalScale's own web app, which was like SquareSpace but for Alexa skills.
+			Since I was changing projects so frequently, I had to learn quickly and communicate effectively with new people.`,
+			technologies: "Android, Alexa, React, Redux, jQuery, node.js",
+			imageUrl: "assets/images/tribalscale.png"
 		},
 		{
 			id: "work1",
 			title: "Imagine Communications",
+			jobTitle: "Full Stack Web Developer",
 			timeline: "January 2017 - April 2017 (2A)",
-			description: `I worked as a full-stack web developer.
-				Primarily, I worked on the web UI for a content management application using AngularJS and LESS.
-				This sometimes took me into difficult territory such as scope manipulation but I still managed to do it.
-				Also, I updated some of the UI's unit tests.
-				My team was Agile, so I learnt to coordinate smoothly with the other developers and QA.
-				Over the four months, I completed almost 100 stories/bugs which, my manager told me, was quite impressive.`,
-			technologies: "AngularJS, Typescript, LESS, Mercurial",
+			description: `I worked on a content management web app using AngularJS and LESS.
+				I worked well with my team and completed almost 100 stories over the four months.
+				Also, I updated many of the UI's unit tests.`,
+			technologies: "AngularJS, Typescript, LESS",
 			imageUrl: "assets/images/imagine-communications.png"
 		},
 		{
 			id: "work2",
 			title: "Metal Trading Co.",
+			jobTitle: "Front-end Web Developer",
 			timeline: "May 2016 - July 2016 (1B)",
-			description: `I worked remotely for this start-up as a front-end web developer.
-				I did this part-time during my first co-op because I felt the day job wasn't challenging enough.
-				When I was working on this, we used Angular2 which was quite new so I learnt to adapt quickly and figure things out with limited resources.
-				We used socket.io for web sockets (as it was a real-time trading platform) and I wrote the node.js server to handle the socket channels.
-				By the end of the three months, I created a working prototype mostly independently.`,
+			description: `I took this part-time job during my first co-op because I felt the co-op wasn't challenging enough.
+				I created a prototype for a real-time trading platform in Angular2.
+				Angular2 was still in rc at the time so I had to learn and adapt quickly with limited resources.
+				In addition, I wrote the web socket part of the server using node.js and socket.io.`,
 			technologies: "Angular2, node.js, Typescript, socket.io",
-			imageUrl: "assets/images/placeholder.jpg"
 		},
 		{
 			id: "work3",
 			title: "AAFC",
+			jobTitle: "Computer Programmer",
 			timeline: "May 2016 - September 2016 (1B)",
-			description: `I worked as an application programmer in VB.NET.
-				The application would read, analyze, and graph agricultural data files.
-				I also updated the CANB Nitrogen Model, which was like a big calculation program written in Fortran.
-				I saw that lots of the existing code could be improved, so I rewrote certain parts of it.
-				In addition, I took the initiative to add useful features when I thought of them, such as adding more automation to the testing program.`,
+			description: `I worked on an graphing application in VB.NET.
+				I also updated a scientific model written in Fortran.
+				When I had time, I took the intiative to add features and improve the existing code.`,
 			technologies: "VB.NET, Fortran",
 			imageUrl: "assets/images/aafc.jpeg"
 		},
@@ -73,35 +73,35 @@ export class OtherWorkComponent {
 			{
 				id: "workcoupletbot",
 				title: "Reddit Couplet Bot",
-				timeline: "",
-				description: '',
-				technologies: "",
-				imageUrl: "assets/images/placeholder.jpg"
+				description: `A bot that parses Reddit comments for couplets.
+				This bot interprets a couplet as two valid English lines that have the same number of syllables and also rhyme.
+				Algorithms involving language are typically very difficult to implement, so I used heuristics and external databases.`,
+				sourceUrl: 'https://github.com/YifanM/reddit-couplet-bot',
+				technologies: "Python",
+				imageUrl: 'assets/images/couplet-bot.png'
 			},
 			{
 				id: "worknotehead",
 				title: "Note Heads",
-				timeline: "",
-				description: '',
-				technologies: "",
-				imageUrl: "assets/images/placeholder.jpg"
+				description: `A note-taking app that uses the same idea as Facebook Messenger's chat heads.
+				I use my phone to keep reminders so I created this app because I wanted to view them from any screen.
+				Although this project is in React Native, there is currently no iOS component.`,
+				sourceUrl: 'https://github.com/YifanM/note-heads',
+				technologies: "React Native, Redux",
 			},
 			{
 				id: "workinvenbot",
 				title: "Inventory Slackbot",
-				timeline: "",
-				description: '',
-				technologies: "",
-				imageUrl: "assets/images/placeholder.jpg"
+				description: `This is an independent project I started at TribalScale.
+				It interacts with the company's inventory system to allow people to view and request devices through Slack.`,
+				technologies: "node.js",
+				imageUrl: 'assets/images/slackbot.png'
 			},
 			{
 				id: "work4",
 				title: "This website",
-				timeline: "April 2017 - Now (In progress)",
 				description: `My current website in Angular2.
-					Its purpose is kind of like a bigger resume. 
-					I use external CSS libraries such as animate.css and Bootstrap, but a lot of my custom CSS is used as well.
-					In the future, I plan to add features like a comments section.`,
+				Currently, I use it to showcase my past work.`,
 				technologies: "Angular2",
 				imageUrl: "assets/images/website1.png",
 				sourceUrl: "https://github.com/YifanM/professional-website-yifan"
@@ -111,12 +111,8 @@ export class OtherWorkComponent {
 			{
 				id: "work5",
 				title: "Gomoku",
-				timeline: "February 2017 - March 2017",
-				description: `A two-player board game.
-					If you're not familiar with Gomoku (not Go), it's like tic-tac-toe but with 5-in-a-row instead.
-					The board I made has three layers.
-					One is responsible for the background grid, one is responsible to animate and display the pieces, and one is responsible to receive clicks in order to update the game state.`,
-				technologies: "ReactJS",
+				description: `A 5-in-a-row board game to introduce myself to React.`,
+				technologies: "React",
 				imageUrl: "assets/images/gomoku.png",
 				sourceUrl: "https://github.com/YifanM/gomoku-react",
 				demoUrl: "assets/gomoku/index.html",
@@ -124,58 +120,45 @@ export class OtherWorkComponent {
 			{
 				id: "work6",
 				title: "YifanM.github.io",
-				timeline: "May 2016 - September 2016",
-				description: `This was my first try at web development, which is why the quality is not that high.
-					Most of the content has been moved to this website.
-					However, it seems the services I used (like a dictionary API) still work.`,
-				technologies: "JQuery, JavaScript, HTML, CSS",
+				description: `This was my first project in web development.
+					I used it as a sandbox to try different web technologies.`,
+				technologies: "JQuery",
 				imageUrl: "assets/images/website2.png",
 				sourceUrl: "https://github.com/YifanM/YifanM.github.io",
 				demoUrl: "http://YifanM.github.io"
 			},
 			{
-				id: "work7",
-				title: "Battle Pong",
-				timeline: "March 2017 - Now (In progress)",
-				description: `My first Android application.
-					It's based on pong, but with a lot of extra features.
-					For example, you can collect a weapon that will paralyze your opponent or spawn more balls.
-					I still want to add a two player online mode and an AI for single player mode.`,
-				technologies: "Java, XML, Android",
-				imageUrl: "assets/images/battle-pong.png",
-				sourceUrl: "https://github.com/YifanM/battle-pong"
-			},
-			{
 				id: "work8",
 				title: "Adoorable",
-				timeline: "April 2017",
-				description: `A simple single-player game with a twist at the end.
-					The levels are designed to spell a message using the player's path.
-					It's pretty much an exact copy of the original, which you can find by searching the same name.`,
-				technologies: "HTML5 Canvas, JQuery, JavaScript",
+				description: `A simple single-player game that has a twist at the end.
+					This is a copy of the game with the same name.`,
+				technologies: "HTML5 Canvas, JQuery",
 				imageUrl: "assets/images/adoorable.png",
 				sourceUrl: "https://github.com/YifanM/adoorable",
 				demoUrl: "assets/adoorable/adoorable.html"
+			},
+			{
+				id: "work7",
+				title: "Battle Pong",
+				description: `My first Android app. It's pong but you can also shoot your opponent.`,
+				technologies: "Java, XML, Android",
+				imageUrl: "assets/images/battle-pong.png",
+				sourceUrl: "https://github.com/YifanM/battle-pong"
 			},
 		],
 		[
 			{
 				id: "work9",
 				title: "InDesign",
-				timeline: "Various",
-				description: `The colourful poster was for a meeting where my high school decided which charity to support.
-					The word poster was for the back page of a yearbook.`,
+				description: `Various InDesign posters.`,
 				technologies: "InDesign",
 				imageUrl: "assets/indesign-pdfs/2.jpg"
 			},
 			{
 				id: "work10",
 				title: "Doodlejump",
-				timeline: "October 2015",
-				description: `My class was given some Arduino-like hardware devices.
-					Some of my classmates and me decided to recreate Doodlejump.
-					We tried a few other small programs, like a calculator that connects to a computer.
-					The program was written in Energia and used hardware features such as the OLED and the accelerometer.`,
+				description: `Doodlejump is a mobile game involving a character controlled through tilting.
+					Some classmates and me recreated it on a LaunchPad using its OLED and the accelerometer.`,
 				technologies: "Tiva-C LaunchPad, Energia",
 				imageUrl: "assets/images/doodlejump.jpg",
 				sourceUrl: "https://github.com/YifanM/orbit-demos"
@@ -183,35 +166,17 @@ export class OtherWorkComponent {
 			{
 				id: "work11",
 				title: "Rainmeter",
-				timeline: "July 2016",
-				description: `Rainmeter is a program for customizing your desktop.
-					It maintains a "skin", which is a layer on the desktop.
-					My skin does things like show the time, weather, and a calendar showing the current day.
-					It can control music players too, which requires a small amount of Lua.`,
+				description: `Rainmeter is a platform for desktop customization.
+					My Rainmeter skin shows some simple meters, such as time and weather.`,
 				technologies: "Rainmeter, Lua",
 				imageUrl: "assets/images/rainmeter.png"
 			},
 			{
-				id: "work12",
-				title: "Game of Life",
-				timeline: "March 2017 - Now (Inactive)",
-				description: `Conway's Game of Life using OpenGL.
-					OpenGL in VS was harder to setup and use than I anticipated, which is why I kind of lost interest.
-					I'll come back to this some day.`,
-				technologies: "OpenGL, C",
-				sourceUrl: "https://github.com/YifanM/game-of-life",
-				imageUrl: "assets/images/placeholder.jpg"
-			},
-		],
-		[
-			{
 				id: "work13",
 				title: "BonaMat",
-				timeline: "July 2014",
-				description: `A project I did while in Shad Valley.
-					My group's idea was a blanket with sensors that would analyze your body metrics and control a smart thermostat accordingly.
-					The blanket contained an Arduino.
-					The code for the demo, which I wrote, was simple: read the sensors, do some calculations, display the data.`,
+				description: `A project I did in Shad Valley.
+				The idea was a mat that read the user's body temperature and communicated it to a smart thermostat.
+				The purpose was to adjust room temperature accordingly while the user slept.`,
 				technologies: "Arduino",
 				imageUrl: "assets/images/bonamat.jpg"
 			},
--- src/app/uw/app.uw.ts
+++ src/app/uw/app.uw.ts
@@ -17,53 +17,58 @@ declare interface ICourseItem {
 export class UWComponent {
 	public courseItems: ICourseItem[] = [
 		{
+			code: "CS 240",
+			name: "Data Structures and Data Management",
+			term: "2B",
+			description: "Various data structures and their operations",
+		},
+		{
+			code: "CS 247",
+			name: "Software Engineering Principles",
+			term: "2B",
+			description: "Software design, concepts, and patterns",
+		},
+		{
 			code: "CS 241",
 			name: "Foundations of Sequential Programs",
 			term: "2A",
 			description: "Scanning, parsing, type-checking, formal languages, automatons etc. Built assembler and compiler for a subset of C++. MIPS architecture.",
-			url: "http://www.ucalendar.uwaterloo.ca/1718/COURSE/course-CS.html#CS241"
 		},
 		{
 			code: "ECE 222",
 			name: "Digital Computers",
 			term: "2A",
 			description: "Explanations on how computer hardware works. ARM architecture.",
-			url: "http://www.ucalendar.uwaterloo.ca/1718/COURSE/course-ECE.html#ECE222"
 		},
 		{
 			code: "SE 212",
 			name: "Logic and Computation",
 			term: "2A",
 			description: "Formal logic applied to program correctness.",
-			url: "http://www.ucalendar.uwaterloo.ca/1718/COURSE/course-SE.html#SE212"
 		},
 		{
 			code: "CS 138",
 			name: "Introduction to Data Abstraction and Implementation",
 			term: "1B",
 			description: "Important data structures and various implementations of them in C++.",
-			url: "http://www.ucalendar.uwaterloo.ca/1718/COURSE/course-CS.html#CS138"
 		},
 		{
 			code: "ECE 124",
 			name: "Digital Circuits and Systems",
 			term: "1B",
 			description: "Boolean algebra, synchronous and asynchronous computer circuits.",
-			url: "http://www.ucalendar.uwaterloo.ca/1718/COURSE/course-ECE.html#ECE124"
 		},
 		{
 			code: "CS 137",
 			name: "Programming Principles",
 			term: "1A",
 			description: "Key concepts of C such as dynamic memory and strings.",
-			url: "http://www.ucalendar.uwaterloo.ca/1718/COURSE/course-CS.html#CS137"
 		},
 		{
 			code: "SE 101",
 			name: "Introduction to Methods of Software Engineering",
 			term: "1A",
 			description: "Light seminars to introduce ideas such as machine learning and encryption.",
-			url: "http://www.ucalendar.uwaterloo.ca/1718/COURSE/course-SE.html#SE101"
 		}
 	]
 }
