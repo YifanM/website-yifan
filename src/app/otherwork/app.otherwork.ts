ee3c69ba72e5bdc318acb126e0300b84890292ad
YifanM <yifan.ma@hotmail.com>
2018-01-13 02:24:11 -0500

    updated content

--- src/app/otherwork/app.otherwork.ts
+++ src/app/otherwork/app.otherwork.ts
@@ -28,9 +28,21 @@ export class OtherWorkComponent {
 			title: "TribalScale",
 			jobTitle: "Agile Software Engineer",
 			timeline: "September 2017 - December 2017 (2B)",
-			description: `I was able to work on various projects such as NBA's Alexa skill, Carnival's Android app, and CIBC's website.
-			I was also able to work on TribalScale's own web app, which was like SquareSpace but for Alexa skills.
-			Since I was changing projects so frequently, I had to learn quickly and communicate effectively with new people.`,
+			description: `I was able to work on many different projects at TribalScale.
+
+One of them was a web app which was like SquareSpace but for Alexa skills. \
+I was able to participate in architecture decisions like the database schema. \
+We designed the app to be highly extensible since we expected to support more types of Alexa skills in the future.
+
+Another project was an Android app for a cruise ship line. \
+I worked on the notifications system and dining reservations. \
+Since it was a legacy app, we were slowly converting it to use better practices.
+
+The third project was an Alexa skill. I helped polish it up. \
+I also made a mock API to help QA, since the live API made it difficult to test all the different flows.
+
+The last project was a web page. \
+There was nothing special about it, but we added many accessiblity features.`,
 			technologies: "Android, Alexa, React, Redux, jQuery, node.js",
 			imageUrl: "assets/images/tribalscale.png"
 		},
@@ -39,9 +51,14 @@ export class OtherWorkComponent {
 			title: "Imagine Communications",
 			jobTitle: "Full Stack Web Developer",
 			timeline: "January 2017 - April 2017 (2A)",
-			description: `I worked on a content management web app using AngularJS and LESS.
-				I worked well with my team in order to complete almost 100 stories over the four months.
-				Also, I updated many of the UI's unit tests.`,
+			description: `I worked on the front-end of a content management web app using AngularJS and LESS. \
+Mostly, I added small features, fixed bugs, and polished the UI. Also, I updated many of the UI's unit tests.
+
+The app I was working on was part of an application suite. \
+I created a messaging service that could be used in each app in the suite. \
+This service would use web sockets to allow the apps to subscribe to messages. \
+Each app could also publish messages in order to communicate with each other. \
+The messaging service also came with a front-end component to show notifications to the user.`,
 			technologies: "AngularJS, Typescript, LESS",
 			imageUrl: "assets/images/imagine-communications.png"
 		},
@@ -50,10 +67,10 @@ export class OtherWorkComponent {
 			title: "Metal Trading Co.",
 			jobTitle: "Front-end Web Developer",
 			timeline: "May 2016 - July 2016 (1B)",
-			description: `I took this part-time job during my first co-op because I felt the co-op wasn't challenging enough.
-				I created a prototype for a real-time trading platform in Angular2.
-				Angular2 was still in rc at the time so I had to learn and adapt quickly with limited resources.
-				In addition, I wrote the web socket part of the server using node.js and socket.io.`,
+			description: `I took this part-time job during my first co-op for a challenge. \
+I designed and created a prototype for a real-time trading app in Angular2. \
+Angular2 was still in rc at the time so I had to learn and adapt quickly with limited resources. \
+In addition, I wrote the server that facilitated the real-time trading feature using node.js and socket.io.`,
 			technologies: "Angular2, node.js, Typescript, socket.io",
 		},
 		{
@@ -61,9 +78,11 @@ export class OtherWorkComponent {
 			title: "AAFC",
 			jobTitle: "Computer Programmer",
 			timeline: "May 2016 - September 2016 (1B)",
-			description: `I worked on an graphing application in VB.NET.
-				I also updated a scientific model written in Fortran.
-				When I had time, I took the initiative to add features and improve the existing code.`,
+			description: `I worked on an graphing application in VB.NET. \
+I added more analytic capabilities to it. \
+If I finished early, I took the initiative to resolve performance issues and refactor the codebase. \
+In addition, I created an application that would automate the testing process. \
+I also updated a scientific model written in Fortran.`,
 			technologies: "VB.NET, Fortran",
 			imageUrl: "assets/images/aafc.jpeg"
 		},
@@ -73,9 +92,9 @@ export class OtherWorkComponent {
 			{
 				id: "workcoupletbot",
 				title: "Reddit Couplet Bot",
-				description: `A bot that parses Reddit comments for couplets.
-				This bot interprets a couplet as two valid English lines that have the same number of syllables and also rhyme.
-				Algorithms involving language are typically very difficult to implement, so I used heuristics and APIs.`,
+				description: `A bot that parses Reddit comments with regex for couplets. \
+This bot interprets a couplet as two valid English lines that have the same number of syllables and also rhyme. \
+The algorithms to count syllables and determine rhymes are very difficult to implement, so I used heuristics and APIs.`,
 				sourceUrl: 'https://github.com/YifanM/reddit-couplet-bot',
 				technologies: "Python",
 				imageUrl: 'assets/images/couplet-bot.png'
@@ -83,26 +102,26 @@ export class OtherWorkComponent {
 			{
 				id: "worknotehead",
 				title: "Note Heads",
-				description: `A note-taking app that uses the same idea as Facebook Messenger's chat heads.
-				The chat heads part requires native code.
-				I use my phone to keep reminders so I created this app because I wanted to view my reminders from any screen.
-				Although this project is in React Native, there is currently no iOS component.`,
+				description: `A note-taking app that uses "chat heads" so notes are easily accessible. \
+Drawing views on the foreground required native code. \
+Although this project is in React Native, there is currently no iOS component.`,
 				sourceUrl: 'https://github.com/YifanM/note-heads',
 				technologies: "React Native, Redux",
 			},
 			{
 				id: "workinvenbot",
 				title: "Inventory Slackbot",
-				description: `This is an independent project I started at TribalScale.
-				It interacts with the company's inventory system to allow people to view and request devices through Slack.`,
+				description: `This is an independent project I started at TribalScale. \
+It interacts with the company's inventory server to allow people to view and request devices through Slack. \
+The node server uses a module to support email interactions as well.`,
 				technologies: "node.js",
 				imageUrl: 'assets/images/slackbot.png'
 			},
 			{
 				id: "work4",
 				title: "This website",
-				description: `My current website in Angular2.
-				Currently, I use it to showcase my past work.`,
+				description: `My personal website that I built in Angular2. I use it to showcase my past work. \
+Although I use a library for the collapsible work items, the rest is basically made from scratch.`,
 				technologies: "Angular2",
 				imageUrl: "assets/images/website1.png",
 				sourceUrl: "https://github.com/YifanM/professional-website-yifan"
@@ -112,7 +131,7 @@ export class OtherWorkComponent {
 			{
 				id: "work5",
 				title: "Gomoku",
-				description: `A 5-in-a-row board game to introduce myself to React.`,
+				description: `A 5-in-a-row board game to introduce myself to React. Supports illegal move detection.`,
 				technologies: "React",
 				imageUrl: "assets/images/gomoku.png",
 				sourceUrl: "https://github.com/YifanM/gomoku-react",
@@ -121,8 +140,7 @@ export class OtherWorkComponent {
 			{
 				id: "work6",
 				title: "YifanM.github.io",
-				description: `This was my first project in web development.
-					I used it as a sandbox to try different web technologies.`,
+				description: `This was my first project in web development. I used it as a sandbox to try different web technologies.`,
 				technologies: "JQuery",
 				imageUrl: "assets/images/website2.png",
 				sourceUrl: "https://github.com/YifanM/YifanM.github.io",
@@ -131,8 +149,7 @@ export class OtherWorkComponent {
 			{
 				id: "work8",
 				title: "Adoorable",
-				description: `A simple single-player game that has a twist at the end.
-					This is a copy of the game with the same name.`,
+				description: `A simple single-player game that has a twist at the end. This is a copy of the game with the same name.`,
 				technologies: "HTML5 Canvas, JQuery",
 				imageUrl: "assets/images/adoorable.png",
 				sourceUrl: "https://github.com/YifanM/adoorable",
@@ -158,8 +175,8 @@ export class OtherWorkComponent {
 			{
 				id: "work10",
 				title: "Doodlejump",
-				description: `Doodlejump is a mobile game involving a character controlled through tilting.
-					Some classmates and me recreated it on a LaunchPad using its OLED and the accelerometer.`,
+				description: `Doodlejump is a mobile game involving a character controlled through tilting. \
+Some classmates and me recreated it on a LaunchPad using its OLED and the accelerometer.`,
 				technologies: "Tiva-C LaunchPad, Energia",
 				imageUrl: "assets/images/doodlejump.jpg",
 				sourceUrl: "https://github.com/YifanM/orbit-demos"
@@ -167,8 +184,8 @@ export class OtherWorkComponent {
 			{
 				id: "work11",
 				title: "Rainmeter",
-				description: `Rainmeter is a platform for desktop customization.
-					My Rainmeter skin shows some simple meters, such as time and weather.`,
+				description: `Rainmeter is a platform for desktop customization. \
+My Rainmeter skin shows some simple meters, such as time and weather.`,
 				technologies: "Rainmeter, Lua",
 				imageUrl: "assets/images/rainmeter.png"
 			},
@@ -176,8 +193,8 @@ export class OtherWorkComponent {
 				id: "work13",
 				title: "BonaMat",
 				description: `A project I did in Shad Valley.
-				The idea was a mat that read the user's body temperature and communicated it to a smart thermostat.
-				The purpose was to adjust room temperature accordingly while the user slept.`,
+				The idea was a mat that read the user's body temperature and communicated it to a smart thermostat. \
+The purpose was to adjust room temperature accordingly while the user slept.`,
 				technologies: "Arduino",
 				imageUrl: "assets/images/bonamat.jpg"
 			},
